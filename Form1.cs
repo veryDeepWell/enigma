2f3e63cc276f797f6556e3efac5ad9cce29bcce4
veryDeepWell <145202229+veryDeepWell@users.noreply.github.com>
2024-02-06 20:00:25 +0300

    06.02.2024

--- Form1.cs
+++ Form1.cs
@@ -6,7 +6,6 @@
 //ring spin
 //three rings
 //decryption
-//test for git )))
 
 using System;
 
@@ -16,37 +15,19 @@ namespace testForEnigma
     {
         Random rnd = new Random();
 
-        public string[] ringFirstChars =
-        {
-            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", " "
-        };
+        public string[] ringFirstLetters     = { "A" , "B" , "C" , "D" , "E" , "F" , "G" , "H" , "I" , "J" , "K" , "L" , "M" , "N" , "O" , "P" , "Q" , "R" , "S" , "T" , "U" , "V" , "W" , "X" , "Y" , "Z" , " "  };
+        public string[] ringFirstPosWires    = { "23", "11", "5" , "10", "1" , "6" , "13", "19", "8" , "4" , "3" , "9" , "26", "25", "12", "21", "18", "20", "15", "16", "24", "2" , "7" , "14", "17", "22", "27" };
+        
+        public string[] ringSecondLetters    = { "A" , "B" , "C" , "D" , "E" , "F" , "G" , "H" , "I" , "J" , "K" , "L" , "M" , "N" , "O" , "P" , "Q" , "R" , "S" , "T" , "U" , "V" , "W" , "X" , "Y" , "Z" , " "  };
+        public string[] ringSecondPosWires   = { "7" , "14", "5" , "4" , "23", "20", "10", "17", "9" , "26", "13", "8" , "25", "19", "3" , "1" , "24", "6" , "18", "12", "21", "11", "16", "2" , "22", "15", "27" };
 
-        public string[] ringFirstNums =
-        { 
-            "23", "11", "5", "10", "1", "6", "13", "19", "8", "4", "3", "9", "26", "25", "12", "21", "18", "20", "15", "16", "24", "2", "7", "14", "17", "22", "27"
-        };
+        public string[] ringThirdLetters     = { "A" , "B" , "C" , "D" , "E" , "F" , "G" , "H" , "I" , "J" , "K" , "L" , "M" , "N" , "O" , "P" , "Q" , "R" , "S" , "T" , "U" , "V" , "W" , "X" , "Y" , "Z" , " "  };
+        public string[] ringThirdNums        = { "23", "11", "5" , "10", "1" , "6" , "13", "19", "8" , "4" , "3" , "9" , "26", "25", "12", "21", "18", "20", "15", "16", "24", "2" , "7" , "14", "17", "22", "27" };
 
-        public string[] ringSecondChars = new string[]
-        {
-            "C", "T", "F", "K", "V", "W", "D", "H", "E", "I", "Y", "P", "N", "Q", "B", "U", "S", "M", "J", "A", "L", "G", "R", "Z", "X", "O", " "
-        };
+        public string[] reflectorFirstLayer  = { "A" , "B" , "C" , "D" , "E" , "F" , "G" , "H" , "I" , "J" , "K" , "L" , "M" , "N" , "O" , "P" , "Q" , "R" , "S" , "T" , "U" , "V" , "W" , "X" , "Y" , "Z" , " "  };
+        public string[] reflectorSecondLayer = { "A" , "B" , "C" , "D" , "E" , "F" , "G" , "H" , "I" , "J" , "K" , "L" , "M" , "N" , "O" , "P" , "Q" , "R" , "S" , "T" , "U" , "V" , "W" , "X" , "Y" , "Z" , " "  };
 
-        public string[] ringSecondNums =
-        { 
-            "7", "14", "5", "4", "23", "20", "10", "17", "9", "26", "13", "8", "25", "19", "3", "1", "24", "6", "18", "12", "21", "11", "16", "2", "22", "15", "27"
-        };
-
-        public string[] ringThirdChars = new string[]
-        {
-            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", " "
-        };
-
-        public string[] ringThirdNums =
-        { 
-            "23", "11", "5", "10", "1", "6", "13", "19", "8", "4", "3", "9", "26", "25", "12", "21", "18", "20", "15", "16", "24", "2", "7", "14", "17", "22", "27"
-        };
-
-        public Form1()
+        public Form1() 
         {
             InitializeComponent();
         }
@@ -63,36 +44,24 @@ namespace testForEnigma
             //uniqueAlert(ringThirdNums, "THIRD");
 
             string textEntered = textBox1.Text;
-            string[] firstEncryptPoses = new string[textEntered.Length];
+            string encryptedText = "";
 
-            for (int i = 0; i < textEntered.Length; i++)
+            foreach (char letter in textEntered)
             {
-                textBox2.Text += nextRingEncrypt(ringSecondChars, ringSecondNums, encryptLetter(ringFirstChars, ringFirstNums, textEntered[i].ToString()));
+                //string firstEncryption  = encryptLetter(ringFirstLetters, ringFirstPosWires, letter.ToString());
+                //string secondEncryption = encryptLetter(ringFirstLetters, ringFirstPosWires, firstEncryption);
+                //string thirdEncryption  = encryptLetter(ringFirstLetters, ringFirstPosWires, secondEncryption);
             }
         }
 
-        private string encryptLetter(string[] _ringChars, string[] _ringNums, string _letterToEncrypt)
+        private void encryptLetter(string[] _ringChars, string[] _ringNums, string _letterToEncrypt)
         {
-            string letterToEncrypt = _letterToEncrypt;
-            string[] ringChars = _ringChars;
-            string[] ringNums = _ringNums;
-
-            int positionOfLetterInRing = Array.IndexOf(ringChars, letterToEncrypt);
-            string ringNumberByPosition = ringNums[positionOfLetterInRing];
-
-            return ringNumberByPosition;
+            
         }
 
-        private string nextRingEncrypt(string[] _nextRingChars, string[] _nextRingNums, string _indexOfPrevLetter)
+        private void nextRingEncrypt(string[] _nextRingChars, string[] _nextRingNums, string _indexOfPrevLetter)
         {
-            string indexOfPrevLetter = _indexOfPrevLetter;
-            string[] nextRingChars = _nextRingChars;
-            string[] nextRingNums = _nextRingNums;
-
-            int positionOfNumberInRing = Array.IndexOf(nextRingNums, indexOfPrevLetter);
-            string ringLetterByPosition = nextRingChars[positionOfNumberInRing];
-
-            return ringLetterByPosition;
+            
         }
 
         //for library
